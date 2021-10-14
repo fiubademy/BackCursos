ed0ef8ce1c17e6e00e15e466d8572a3e1ba55896
tomasarrachea <tarrachea@fi.uba.ar>
2021-10-14 15:53:37 -0300

    CRUD funcionando con db

--- service/CourseService.py
+++ service/CourseService.py
@@ -49,11 +49,78 @@ class CourseResponse(BaseModel):
     courseName: str
 
 
+course_students = Table('course_students', Base.metadata,
+                        Column('course_id', ForeignKey(
+                            'courses.id'), primary_key=True),
+                        Column('student_id', ForeignKey(
+                            'students.user_id'), primary_key=True)
+                        )
+
+course_hashtags = Table('course_hashtags', Base.metadata,
+                        Column('course_id', ForeignKey(
+                            'courses.id'), primary_key=True),
+                        Column('hashtag_id', ForeignKey(
+                            'hashtags.id'), primary_key=True)
+                        )
+
+course_teachers = Table('course_teachers', Base.metadata,
+                        Column('course_id', ForeignKey(
+                            'courses.id'), primary_key=True),
+                        Column('teacher_id', ForeignKey(
+                            'teachers.user_id'), primary_key=True)
+                        )
+
+
 class Course(Base):
     __tablename__ = "courses"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     description = Column(String)
+    content = relationship('Content', back_populates="course",
+                           cascade="all, delete, delete-orphan")
+    students = relationship('Student',
+                            secondary=course_students,
+                            back_populates='courses')
+    hashtags = relationship('Hashtag',
+                            secondary=course_hashtags,
+                            back_populates='courses')
+    teachers = relationship('Teacher',
+                            secondary=course_teachers,
+                            back_populates='courses')
+
+
+class Student(Base):  # many to many relationship
+    __tablename__ = "students"
+    user_id = Column(UUID(as_uuid=True), primary_key=True,
+                     default=uuid.uuid4)  # TODO CONECTAR CON DB DE USUARIOS
+    courses = relationship('Course',
+                           secondary=course_students,
+                           back_populates='students')
+
+
+class Hashtag(Base):  # many to many relationship
+    __tablename__ = "hashtags"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    tag = Column(String(140), nullable=False, unique=True)
+    courses = relationship('Course',
+                           secondary=course_hashtags,
+                           back_populates='hashtags')
+
+
+class Teacher(Base):  # many to many relationship
+    __tablename__ = "teachers"
+    user_id = Column(UUID(as_uuid=True), primary_key=True,
+                     default=uuid.uuid4)  # TODO CONECTAR CON DB DE USUARIOS
+    courses = relationship('Course',
+                           secondary=course_teachers,
+                           back_populates='teachers')
+
+
+class Content(Base):  # one to many relationship
+    __tablename__ = "content"
+    id = Column(Integer, primary_key=True)
+    course_id = Column(UUID(as_uuid=True), ForeignKey('courses.id'))
+    course = relationship("Course", back_populates="content")
 
 
 @app.get('/courses', response_model=List[CourseResponse], status_code=status.HTTP_200_OK)
